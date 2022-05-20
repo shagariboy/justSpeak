5f37f84ced2b86ac8b050a659175a1388de96d9a
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2022-05-20 09:27:43 +0100

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).on("click", ".navbar-right .dropdown-menu", function(e){
 	e.stopPropagation();
 });
+
+
+window.onload = function (g_id_onload) {
+    google.accounts.id.initialize({
+      client_id: '270686179289-u2m5huooq9ljcfibktb4akibg5omm0o0.apps.googleusercontent.com',
+      callback: handleCredentialResponse
+    });
+    google.accounts.id.prompt();
+  };
